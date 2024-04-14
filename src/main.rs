2df3c7480c320e886171e7fdd796f3f42641ed34
Agustin Castillo <agustincasti2002@gmail.com>
2024-04-13 21:48:50 -0300

    Practica 2 terminada

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod tp02;
 fn main() {
-    let mut num = 1.1;
-    tp02::ej14::incrementar(&mut num);
-    println!("{}", num);
+    let mut n = 1.1;
+    tp02::ej14::incrementar(&mut n);
+    println!("{}", n);
 }
